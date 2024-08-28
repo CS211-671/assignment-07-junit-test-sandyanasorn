df8ef7e24ccd0abd8403495cbffaf8b7e1bcaf7d
Yanasorn <yanasorn.sa@ku.th>
2024-08-28 19:59:03 +0700

    finish test model

--- src/test/java/ku/cs/models/StudentListTest.java
+++ src/test/java/ku/cs/models/StudentListTest.java
@@ -2,27 +2,63 @@ package ku.cs.models;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentListTest {
     @Test
-    void testAddNewStudentByIdAndName(){
+    public void testAddNewStudentWithoutScore() {
         StudentList studentList = new StudentList();
         studentList.addNewStudent("1", "a");
-        Student student = studentList.findStudentById("1");
-        assertEquals("a", student.getName());
+        ArrayList<Student> students = studentList.getStudents();
+
+        assertEquals(1, students.size());
+        assertEquals("1", students.get(0).getId());
+        assertEquals("a", students.get(0).getName());
+        assertEquals(0.0, students.get(0).getScore());
     }
 
     @Test
-    void testAddNewStudentByIdNameAndScore(){
+    public void testAddNewStudentWithScore() {
         StudentList studentList = new StudentList();
-        studentList.addNewStudent("1", "a", 10);
-        Student student = studentList.findStudentById("1");
-        assertEquals(10, student.getScore());
+        studentList.addNewStudent("2", "b", 100.0);
+        ArrayList<Student> students = studentList.getStudents();
+
+        assertEquals(1, students.size());
+        assertEquals("2", students.get(0).getId());
+        assertEquals("b", students.get(0).getName());
+        assertEquals(100.0, students.get(0).getScore());
     }
 
     @Test
-    void testFindStudentById(){
+    public void testFindStudentById() {
+        StudentList studentList = new StudentList();
+        studentList.addNewStudent("3", "c");
+        Student student = studentList.findStudentById("3");
 
+        assertNotNull(student);
+        assertEquals("3", student.getId());
+        assertEquals("c", student.getName());
+    }
+
+    @Test
+    public void testGiveScoreToId() {
+        StudentList studentList = new StudentList();
+        studentList.addNewStudent("4", "d", 50.0);
+        studentList.giveScoreToId("4", 25.0);
+        Student student = studentList.findStudentById("4");
+
+        assertNotNull(student);
+        assertEquals(75.0, student.getScore());
+    }
+
+    @Test
+    public void testViewGradeOfId() {
+        StudentList studentList = new StudentList();
+        studentList.addNewStudent("5", "e", 92.0);
+        String grade = studentList.viewGradeOfId("5");
+
+        assertEquals("A", grade);
     }
 }
